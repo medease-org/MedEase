6aa2e38e9e5d908ad56e718a112ae66a7ed2ddd8
Prathamesh Naraje <prathamesh.naraje23@gmail.com>
2026-08-05 04:44:10 +0530

    Change Password Request DTO Added

---
+++ medease-backend/src/main/java/com/medeasemanagement/dto/ChangePasswordRequest.java
@@ -0,0 +1,12 @@
+package com.medeasemanagement.dto;
+
+import lombok.Data;
+
+@Data
+public class ChangePasswordRequest {
+    private String email;
+    private String oldPassword;
+    private String newPassword;
+
+    // Getters & Setters
+}
